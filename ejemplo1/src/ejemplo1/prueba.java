48c297ed285e6fb8f45f66aae7d3d188ace18d09
pedronavarrete <pedronavarrete@macbook-pro.home>
2021-03-10 19:08:10 +0100

    commint 1 ejemplo1

---
+++ ejemplo1/src/ejemplo1/prueba.java
@@ -0,0 +1,11 @@
+package ejemplo1;
+
+	
+	public class prueba {
+		
+		// subir a git
+	    public static void main(String[] args) {
+	        // Here goes the statements used by the program
+	        System.out.println("Hello world!");
+	    }
+	}
---
+++ ejemplo1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pedronavarrete
+ *
+ */
+module ejemplo1 {
+}
