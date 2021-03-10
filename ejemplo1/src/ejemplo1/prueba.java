7c34ec9898423c99b8aa380f99c2ff6b27b70556
pedronavarrete <pedronavarrete@macbook-pro.home>
2021-03-10 21:29:27 +0100

    commint 3

--- ejemplo1/src/ejemplo1/prueba.java
+++ ejemplo1/src/ejemplo1/prueba.java
@@ -3,7 +3,7 @@ package ejemplo1;
 	
 	public class prueba {
 		
-		// subir a git
+		// subir a git2
 	    public static void main(String[] args) {
 	        // Here goes the statements used by the program
 	        System.out.println("Hello world!");
