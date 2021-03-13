ef500e8311a557ab752a9956bdc8bb67289b7300
Petusin <petusin1970@gmail.com>
2021-03-13 18:17:31 +0100

    calculadora_metodo_12_03_21_18_17

--- calculadora/src/calculadora/Calculadora_metodo.java
+++ calculadora/src/calculadora/Calculadora_metodo.java
@@ -3,20 +3,94 @@ package calculadora;
 import java.util.Scanner;
 
 public class Calculadora_metodo {
+    private static final String suma = "+";
+    private static final String resta = "-";
+    private static final String multiplica = "*";
+    private static final String divivide = "/";
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    private enum operation {
+        suma, resta, multiplica, divide
+    };
+    private int solution;
+    private int x;
+    public int getX() {
+        return x;
+    }
 
-	    System.out.print("Enter the first number: ");
-	    int number1 = Integer.valueOf(scanner.nextLine());
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	    System.out.print("Enter the second number: ");
-	    int number2 = Integer.valueOf(scanner.nextLine());
+    public int getY() {
+        return y;
+    }
 
-	    System.out.print("The total sum of the numbers is: " + sum(number1, number2));
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public static int sum(int first, int second) {
-	    return first + second;
-	}
+    private int y;
+
+
+
+    static Scanner operators;
+
+    public Calculadora_metodo() {
+        solution = 0;
+        operators = new Scanner(System.in);
+
+    }
+
+    public int addition(int x, int y) {
+        return x + y;
+    }
+
+    public int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public int division(int x, int y) {
+        solution = x / y;
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        Calculadora_metodo calc = new Calculadora_metodo();
+
+        System.out.println("Inserte 2 numeros");
+
+        System.out.println("Numero 1: ");
+
+        calc.setX(Integer.parseInt(operators.next()));
+
+        System.out.println("Numero 2: ");
+        calc.setY(Integer.parseInt(operators.next()));
+
+        System.out.println("What operation? ('suma', 'resta', 'multiplica', 'divide')");
+        operation ttt = operation.valueOf(operators.next());
+        int output = 0 ;
+        switch(ttt){
+        case suma:
+            output = calc.addition(calc.getX(), calc.getY());
+
+            break;
+          case resta:
+              output = calc.subtraction(calc.getX(), calc.getY());
+
+            break;
+          case multiplica:
+              output = calc.multiplication(calc.getX(), calc.getY());
+
+            break;
+          case divide:
+              output = calc.division(calc.getX(), calc.getY());
+
+            break;
+        }
+        System.out.println("Resultado ="+output);
+    }
 }
